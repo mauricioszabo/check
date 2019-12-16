f93b7dc6c36e6a77c6917e2645ba2c85ee0e973f
Maurício Szabo <mauricio.szabo@gmail.com>
2019-12-15 22:38:28 -0300

    Dependencies fix

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.async "0.4.490"]
                  [net.cgrand/macrovich "0.2.1"]
+                 [nubank/matcher-combinators "1.0.1"]
                  [expectations "2.2.0-rc3"]]
 
   :profiles {:dev {:src-paths ["dev"]
-                   :dependencies [[org.clojure/clojure "1.9.0"]
-                                  [thheller/shadow-cljs "2.7.9"]
-                                  [org.clojure/clojurescript "1.10.439"]]}})
+                   :dependencies [[org.clojure/clojure "1.10.1"]
+                                  [thheller/shadow-cljs "2.8.83"]]}})
