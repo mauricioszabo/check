6045fecc5aafd62d8b508f4952dd298c228e9dee
Maurício Szabo <mauricio.szabo@gmail.com>
2019-12-17 16:02:03 -0300

    Bump matcher-combinators

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/core.async "0.4.490"]
                  [net.cgrand/macrovich "0.2.1"]
-                 [nubank/matcher-combinators "1.0.1"]
+                 [nubank/matcher-combinators "1.2.6"]
                  [expectations "2.2.0-rc3"]]
 
   :profiles {:dev {:src-paths ["dev"]
