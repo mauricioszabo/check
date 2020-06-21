6b1527c509a66b8ff9becae261ea5d2abe86e907
Maurício Szabo <mauricio.szabo@gmail.com>
2020-06-21 02:44:18 -0300

    Update matcher-combinators and shadow-cljs

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/mauricioszabo/check"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/core.async "0.4.490"]
+  :dependencies [[org.clojure/core.async "1.2.603"]
                  [net.cgrand/macrovich "0.2.1"]
                  [funcool/promesa "4.0.2"]
-                 [nubank/matcher-combinators "1.2.6"]
+                 [nubank/matcher-combinators "2.1.0"]
                  [expectations "2.2.0-rc3"]]
 
   :profiles {:dev {:src-paths ["dev"]
-                   :dependencies [[org.clojure/clojure "1.10.1"]
-                                  [thheller/shadow-cljs "2.8.83"]]}})
+                   :dependencies [[org.clojure/clojure "1.10.0"]
+                                  [thheller/shadow-cljs "2.10.10"]]}})
