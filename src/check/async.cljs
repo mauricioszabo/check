8aa39357b1331c7e5c26076929da86a272b533e4
Maurício Szabo <mauricio.szabo@gmail.com>
2020-10-01 11:05:58 -0300

    Removed comments

--- src/check/async.cljs
+++ src/check/async.cljs
@@ -3,40 +3,11 @@
   (:require [clojure.string :as str]
             [check.core :as core]
             [clojure.test :as test]
+            [cljs.test]
             [clojure.core.async :as async]
             [promesa.core :as p]
-            ; [net.cgrand.macrovich :as macros]
             [cljs.core.async.impl.protocols :as proto]))
-  ; (:import [cljs.core.async.impl.protocols ReadPort]))
 
-; (defn- to-chan [left]
-;   `(let [chan# (async/promise-chan)]
-;        (.then ~left (fn [result#] (async/put! chan# result#)))
-;        chan#))
-;
-; (defn get-from-channel! [cljs? chan]
-;   (if cljs?
-;    `(async/<! (if (instance? js/Promise ~chan)
-;                 ~(to-chan chan)
-;                 ~chan))
-;    `(first (async/alts!! [~chan (async/timeout timeout)]))))
-;
-; (defmethod core/assert-arrow '=resolves=> [cljs? left _ right]
-;   `(let [value# (get-from-channel! ~cljs? ~left)]
-;      (core/assert-arrow ~cljs? value# '=> ~right)))
-;
-; (defn promise-test* [prom done tear time]
-;   (let [teardown (or tear #())
-;         mark-done (delay (do (teardown) (done)))]
-;     (js/setTimeout (fn [] (when-not (realized? mark-done)
-;                             (throw (ex-info "Async error - not finalized" {}))
-;                             @mark-done))
-;                    time)
-;     (.then prom (fn [_] @mark-done))
-;     (.catch prom (fn [error]
-;                    @done
-;                    (throw (ex-info "Promise resolved with error" {:error error}))))))
-;
 (defn to-promise [promise-or-chan]
   (if (satisfies? proto/ReadPort promise-or-chan)
     (let [p (p/deferred)]
@@ -48,13 +19,28 @@
 
 (defn async-test* [description timeout teardown prom]
   (test/async done
-    (let [timeout-prom (p/do!
-                        (p/delay timeout)
-                        (p/rejected (ex-info "Async test not finalized"
-                                             {:timeout timeout})))]
-      (test/testing description
-        (-> (p/race [prom timeout-prom])
-            (p/catch (fn [error] (test/is (not error)) nil))
-            (p/then (fn [_] (teardown)))
-            (p/catch (fn [error] (test/is (not error)) nil))
+    (test/testing description
+      (let [timeout-error (js/Object.)
+            error (js/Object.)
+            promise (p/catch prom #(vector error %))
+            timeout-prom (p/do!
+                          (p/delay timeout)
+                          timeout-error)]
+        (-> (p/race [promise timeout-prom])
+            (p/then (fn [res]
+                      (cond
+                        (and (vector? res) (-> res first (= error)))
+                        (test/do-report {:type :error
+                                         :expected "Not an error"
+                                         :actual (second res)})
+
+                        (= timeout-error res)
+                        (test/do-report {:type :error
+                                         :expected "Test to finish"
+                                         :actual (str "Test did not finish in " timeout "msec")}))
+
+                      (teardown)))
+            (p/catch (fn [error]
+                       (test/is (not error))
+                       nil))
             (p/finally (fn [ & _] (done))))))))
